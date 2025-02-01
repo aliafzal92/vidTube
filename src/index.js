93fd51173f97351d188809007a778891aa77df99
aliafzal92 <alichaudhary9292@gmail.com>
2025-02-02 00:13:15 +0500

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import 'dotenv/config'
+import "dotenv/config";
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
-import {connectDB} from "./db/index.js";
+const PORT = process.env.PORT || 8000;
 
+connectDB()
+  .then(() => {
+    // Start server only after DB is connected
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err);
+    process.exit(1); // Exit process if DB connection fails
+  });
 
+app.on("error", (err) => {
+  console.error("🔥 Server Error:", err);
+  process.exit(1);
+});
 
-connectDB();
-
-
-
-
-
-
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//     throw error;
-//   }
-// })();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// const asyncHandler = (requestHandler)=>{
+//     return Promise.res
+
+// }
+
+export const asyncHandler = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: "false",
+        message: error.message,
+      });
+    }
+  };
+};
